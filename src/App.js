06a5d4386e968d36dcb33634ac603a7ef0110d7b
Shea Cronin <shealcronin@gmail.com>
2022-04-16 14:42:54 -0400

    Add Header and remove boilerplate README

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import Crop from './components/Crop';
 import crops from './data/crops.json';
+import Header from './components/Header';
 
 function App() {
     return (
-        <section className="crops-container">
-            <h2>Crops</h2>
-            {crops.map((crop) => (
-                <Crop key={crop.name} crop={crop} />
-            ))}
-        </section>
+        <>
+            <Header />
+            <section className="crops-container">
+                <h2>Crops</h2>
+                {crops.map((crop) => (
+                    <Crop key={crop.name} crop={crop} />
+                ))}
+            </section>
+        </>
     );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import '../styles/Header.css';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Rune Factory 5 Tracker</h1>
+        </header>
+    );
+};
+
+export default Header;
